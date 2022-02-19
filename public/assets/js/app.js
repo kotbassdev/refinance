4d46fb892bcd371dcd1352d15f65cb442de5df08
kotbassdev <kotbass.dev@gmail.com>
2022-02-19 11:04:59 +0700

    update style for mobile and tablet

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,15 +1,24 @@
 function clearLinkActive() {
     Array.from(document.querySelectorAll('.nav-item')).map(item => item.querySelector('a').classList.remove('active'))
 }
-function handleLinkClick(id){
-    clearLinkActive()
-    const element = document.getElementById(id)
-    console.log(element)
-    document.getElementById(`link-${id}`).classList.add('active')
+function handleNavbarMenuClick(id){
+    clearLinkActive();
+    const element = document.getElementById(id);
+    document.getElementById(`link-${id}`).classList.add('active');
     element.scrollIntoView({ behavior: 'smooth', block: 'center'});
 }
 function handleLinkMapClick(){
-    console.log('handleLinkMapClick')
-    clearLinkActive()
+    console.log('handleLinkMapClick');
+    clearLinkActive();
     document.getElementById('map').scrollIntoView({ behavior: 'smooth', block: 'center'});
 }
+function handleBtnHamburgerClick(){
+    document.querySelector(".navbar-mobile").classList.toggle('active');
+}
+function handleMobileNavbarMenuClick(id){
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth', block: 'center'});
+    document.querySelector('.navbar-mobile').classList.remove('active');
+}
+
+window.addEventListener("scroll",() => document.querySelector('.btn-uptotop').style.display = window.pageYOffset > 1000 ? 'block' : 'none')
